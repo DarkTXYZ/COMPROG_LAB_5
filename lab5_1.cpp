d5b66079d12969332dd604397e30a6c262801634
DarkTXYZ <pawaretmontfort@gmail.com>
2020-12-15 15:29:15 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+    int input = -1;
+    int cnteven = 0 , cntodd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> input;
+    while(input){
+        if(input % 2 == 0)
+            cnteven++;
+        else
+            cntodd++;
+        cout << "Enter an integer: ";
+        cin >> input;
+    }
+    cout << "#Even numbers = " << cnteven << endl;
+    cout << "#Odd numbers = " << cntodd << endl;
     return 0;
 }
